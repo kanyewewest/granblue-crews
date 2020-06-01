4e28b46f15c746a1a710eefc87c8f178c36d8733
rubberdvcky <dvckyyyy@gmail.com>
2020-06-01 03:04:14 -0500

    feat(~badge): added object input support

--- packages/client/src/app/shared/badge/badge.component.ts
+++ packages/client/src/app/shared/badge/badge.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { BadgeBackgroundColor, BadgeTextColor } from './badge.interface';
+import { BadgeBackgroundColor, BadgeTextColor, Badge } from './badge.interface';
 
 @Component({
   selector: 'app-badge',
@@ -11,8 +11,18 @@ export class BadgeComponent {
   @Input() description = '';
   @Input() backgroundColor = BadgeBackgroundColor.BLUE;
   @Input() borderColor = BadgeBackgroundColor.BLUE;
-  @Input() isBorderDashed = true;
   @Input() textColor = BadgeTextColor.WHITE;
+  @Input() isBorderDashed = false;
+
+  // Use either this setter or the inputs above
+  @Input() set badge(val: Badge) {
+    this.name = val.name;
+    this.description = val.description;
+    this.backgroundColor = val.backgroundColor;
+    this.borderColor = val.borderColor;
+    this.textColor = val.textColor;
+    this.isBorderDashed = val.isBorderDashed;
+  }
 
   constructor() {}
 }
