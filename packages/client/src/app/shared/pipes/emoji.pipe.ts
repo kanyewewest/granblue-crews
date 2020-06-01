26d7d1287359ec063a98f762c900fd909d6717c1
rubberdvcky <dvckyyyy@gmail.com>
2020-06-01 06:57:16 -0500

    feat(emoji): added emoji pipe

---
+++ packages/client/src/app/shared/pipes/emoji.pipe.ts
@@ -0,0 +1,23 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+
+declare var twemoji: {
+  convert: { fromCodePoint(str: string): string };
+  parse(str: string, options?: { folder: string; ext: string }): string;
+};
+
+@Pipe({
+  name: 'emoji',
+})
+export class EmojiPipe implements PipeTransform {
+  constructor(private domSanitizer: DomSanitizer) {}
+  transform(codePoint: string): SafeHtml {
+    const castedCode = twemoji.convert.fromCodePoint(codePoint);
+    return this.domSanitizer.bypassSecurityTrustHtml(
+      twemoji.parse(castedCode, {
+        folder: 'svg',
+        ext: '.svg',
+      }),
+    );
+  }
+}
