219403da6db154cba25f1fe61f157b3122f9a5d0
rubberdvcky <dvckyyyy@gmail.com>
2020-06-06 02:24:16 -0500

    feat(client.layout): integrated with app

--- packages/client/src/app/shared/layout/layout.component.ts
+++ packages/client/src/app/shared/layout/layout.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { HeaderNavItem } from '@gbc/models/header';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.scss']
+  styleUrls: ['./layout.component.scss'],
 })
 export class LayoutComponent implements OnInit {
-
-  constructor() { }
-
+  navItems: HeaderNavItem[];
+  constructor() {}
   ngOnInit(): void {
+    this.navItems = [
+      {
+        name: 'Crews',
+        url: '/crews',
+      },
+      {
+        name: 'Players',
+        url: '/players',
+      },
+    ];
   }
-
 }
